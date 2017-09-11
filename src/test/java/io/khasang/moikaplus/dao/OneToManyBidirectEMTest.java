55b7c4417f456bfb06f7f563e0942e40c242e79f
vol2hv <vol2hv@yandex.ru>
2017-09-11 15:29:32 +0300

    OneToManyBidirectEMTest связь двусторонняя на уровне Entity Manager работает

--- src/test/java/io/khasang/moikaplus/dao/OneToManyBidirectEMTest.java
+++ src/test/java/io/khasang/moikaplus/dao/OneToManyBidirectEMTest.java
@@ -7,10 +7,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
+import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceContext;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,12 +22,11 @@ import java.util.List;
 @SpringBootTest(classes = SpringBootWebApplication.class)
 @Transactional
 //@Rollback
-public class OneToManyBidirectTest {
+//@Commit
+public class OneToManyBidirectEMTest {
+    @PersistenceContext
+    private EntityManager entityManager;
 
-    @Autowired
-    PersonRepository personRepository;
-    @Autowired
-    PhoneRepository phoneRepository;
 
     Person person;
     List<Person> personList = new ArrayList<>();
@@ -31,42 +34,22 @@ public class OneToManyBidirectTest {
 
     @Test
     public void OneToManyBidirect() throws Exception {
-        phoneRepository.deleteAll();
-        personRepository.deleteAll();
+        entityManager.clear();
 
-        Person person = new Person();
-        person.setName("Ivanov");
-        person.addPhone(new Phone(0l,"001",person));
-//        phoneList.add(new Phone(0l,"001",person));
-//        phoneList.add(new Phone(0l,"002",person));
-//        person.setPhones(phoneList);
-        person = personRepository.save(person);
+        Person person = new Person("Мадорин");
+        Phone phone1 = new Phone( "123-456-7890" );
+        Phone phone2 = new Phone( "321-654-0987" );
 
-        personList.clear();
-        phoneList.clear();
-        personRepository.findAll().forEach(personList::add);
-        phoneRepository.findAll().forEach(phoneList::add);
+        person.addPhone( phone1 );
+        person.addPhone( phone2 );
+        entityManager.persist( person );
+        entityManager.flush();
+
+        person.removePhone( phone1 );
+        entityManager.flush();
+        entityManager.close();
+
+
     }
 
 }
-
-//    BookCategory categoryA = new BookCategory("Category A");
-//    Set bookAs = new HashSet<Book>(){{
-//        add(new Book("Book A1", categoryA));
-//        add(new Book("Book A2", categoryA));
-//        add(new Book("Book A3", categoryA));
-//    }};
-//        categoryA.setBooks(bookAs);
-//
-//                BookCategory categoryB = new BookCategory("Category B");
-//                Set bookBs = new HashSet<Book>(){{
-//        add(new Book("Book B1", categoryB));
-//        add(new Book("Book B2", categoryB));
-//        add(new Book("Book B3", categoryB));
-//        }};
-//        categoryB.setBooks(bookBs);
-//
-//        bookCategoryRepository.save(new HashSet<BookCategory>() {{
-//        add(categoryA);
-//        add(categoryB);
-//        }});
